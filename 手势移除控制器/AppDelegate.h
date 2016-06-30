a18bd293e4d214b6777c7bd5eae8819be6e2f2fd
angmu <anglimu@163.com>
2016-06-30 12:19:41 +0800

    inital

---
+++ 手势移除控制器/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  66-手势移除控制器
+//
+//  Created by 穆良 on 16/6/30.
+//  Copyright © 2016年 穆良. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
